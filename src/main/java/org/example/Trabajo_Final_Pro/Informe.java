be0d78e2e373efb0e082272eeffbf5b7ec70200f
EduardoTrujillo <le.trujillo06@ciaf.edu.co>
2024-05-17 20:49:56 -0500

    Proyecto Base Santy

---
+++ src/main/java/org/example/Trabajo_Final_Pro/Informe.java
@@ -0,0 +1,15 @@
+package Trabajo_Final_Pro;
+
+public class Informe {
+
+    public void generarInformeVentas() {
+
+        System.out.println("Generando informe de ventas...");
+    }
+
+
+    public void generarInformeInventario() {
+
+        System.out.println("Generando informe de inventario...");
+    }
+}
---
+++ src/main/java/org/example/Trabajo_Final_Pro/Producto.java
@@ -0,0 +1,45 @@
+package Trabajo_Final_Pro;
+
+public class Producto {
+    private int id;
+    private String nombre;
+    private double precio;
+    private int cantidad;
+
+    public Producto(int id, String nombre, double precio, int cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", cantidad=" + cantidad +
+                '}';
+    }
+}
